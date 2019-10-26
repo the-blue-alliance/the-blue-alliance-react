ddc6afd7f6022c0ca5d5986070fed22e9897c0db
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2019-10-26 19:42:32 -0400

    Fix unregistered RN view, fix event insights background (#38)
    * Set version of Xcode better to try to fix CI
    
    * Update to React Native 0.59.0 (#504)
    
    [beta]
    
    833c4fa RN 0.59.0
    1acfae4 Update 2019 breakdown to use icons (#28)
    
    git-subtree-dir: subtrees/the-blue-alliance-react
    git-subtree-split: 833c4fabf864580c468eb9c6ef5a360754a06482
    
    * [beta] Fix bad RN merge (#627)
    
    * Fix unregistered RN view, fix event insights background

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,13 @@
 'use strict';
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
+
+import MatchBreakdown from './app/views/MatchBreakdown';
+import EventInsights from './app/views/EventInsights';
+
+AppRegistry.registerComponent('MatchBreakdown', () => MatchBreakdown);
+AppRegistry.registerComponent('EventInsights', () => EventInsights);
+
 import MatchBreakdown2015 from './app/views/breakdowns/MatchBreakdown2015';
 import MatchBreakdown2016 from './app/views/breakdowns/MatchBreakdown2016';
 import MatchBreakdown2017 from './app/views/breakdowns/MatchBreakdown2017';
@@ -11,12 +18,18 @@ import EventInsights2017 from './app/views/event-insights/EventInsights2017';
 import EventInsights2018 from './app/views/event-insights/EventInsights2018';
 import EventInsights2019 from './app/views/event-insights/EventInsights2019';
 
+// These old style routes are deprecated - create new routes like the above
 AppRegistry.registerComponent('MatchBreakdown2015', () => MatchBreakdown2015);
 AppRegistry.registerComponent('MatchBreakdown2016', () => MatchBreakdown2016);
 AppRegistry.registerComponent('MatchBreakdown2017', () => MatchBreakdown2017);
 AppRegistry.registerComponent('MatchBreakdown2018', () => MatchBreakdown2018);
 AppRegistry.registerComponent('MatchBreakdown2019', () => MatchBreakdown2019);
+AppRegistry.registerComponent('MatchBreakdown2020', () => <View/>);
+AppRegistry.registerComponent('MatchBreakdown2021', () => <View/>);
+AppRegistry.registerComponent('MatchBreakdown2022', () => <View/>);
 AppRegistry.registerComponent('EventInsights2016', () => EventInsights2016);
 AppRegistry.registerComponent('EventInsights2017', () => EventInsights2017);
 AppRegistry.registerComponent('EventInsights2018', () => EventInsights2018);
-AppRegistry.registerComponent('EventInsights2019', () => EventInsights2019);
+AppRegistry.registerComponent('EventInsights2020', () => <View/>);
+AppRegistry.registerComponent('EventInsights2021', () => <View/>);
+AppRegistry.registerComponent('EventInsights2022', () => <View/>);
